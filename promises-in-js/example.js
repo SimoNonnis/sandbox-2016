4743355d4fc3e5f8dd1d73fa2456fa64b0a4b26e
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2016-06-07 19:09:34 +0200

    Update example.js

--- promises-in-js/example.js
+++ promises-in-js/example.js
@@ -1,62 +1,26 @@
 var fetch = require('node-fetch');
 
-// Let's consider as example the fetch API, which internally creates a new promise object
-// If the fetch API used callback design
-
-// error-first style aka node.js style
-fetch('foo', (err, res) => {
-  if (err) {
-    // handle error
-  }
-
-  // handle response
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(x => resolve(100),2000)
 });
 
+p1.then(result => console.log(result));
 
-// split callback
-function failure(data) {
-  // handle error
-};
+setTimeout(() => p1.then(result => console.log(result * 1000)),6000)
 
-function success(data) {
-  // handle success
-};
+// example of returning a promise, where we randomly fulfill or reject after 3 second.
 
-fetch('foo', failure, success);
+var p2 = Promise.resolve()
+  .then(data => new Promise((resolve, reject) => {
+    setTimeout(Math.random() > 0.1 ? resolve : reject, 3000)
+  }));
 
+p2.then(data => console.log('Okay'));
+p2.catch(data => console.log('Oooops'));
 
-// simplest use case
-fetch('foo').then((res) => {
-  // handle fulfillment
-})
+// first creates a promise fulfilled with [1, 2, 3] and then has a fulfillment // branch on top of that which maps thoes values into [2, 4, 6]. Calling .then // on that branch of the promise will produce the doubled values.
 
+var p3 = Promise.resolve([1,2,3]);
 
-// .then is able to register a reaction to rejections as its second argument
-var p = fetch('foo');
-
-p.then(
-  (res) => {
-    // handle success
-  },
-  (err) => {
-    // handle error
-  }
-);
-
-// .catch takes only rejection (err) as argument
-var p = fetch('foo');
-
-p.then(res => {
-  // handle success
-});
-
-p.catch(err => {
-  // handle error
-});
-
-
-// Creating a Promise From Scratch
-new Promise(resolve => resolve(42))
-  .then(
-    res => console.log(`Hello: ${res}`)
-  );
+p3.then(values => values.map(x => x * 2))
+  .then(values => console.log(values));
