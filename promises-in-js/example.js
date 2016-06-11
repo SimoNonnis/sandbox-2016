f453201b44cc8e70512f587448051478be938682
Simon Nonnis <simononnis@gmail.com>
2016-06-11 18:17:59 +0200

    Promise.all + fetch

--- promises-in-js/example.js
+++ promises-in-js/example.js
@@ -1,26 +1,23 @@
 var fetch = require('node-fetch');
 
-let p1 = new Promise((resolve, reject) => {
-  setTimeout(x => resolve(100),2000)
-});
+var loadUser = (user) => {
+    const ROOT_URL = 'https://api.github.com/users/';
+    return fetch(ROOT_URL + user);
+}
 
-p1.then(result => console.log(result));
 
-setTimeout(() => p1.then(result => console.log(result * 1000)),6000)
+Promise.all([
+  loadUser('SimoNonnis'),
+  loadUser('wesbos')
+])
+  .then(users => {
+    users.map(user => {
+      user.json()
+        .then(user => {
+          const userInfo = `username: ${user.login} \nbio: ${user.bio} \nwebsite: ${user.blog}\n\n`
 
-// example of returning a promise, where we randomly fulfill or reject after 3 second.
-
-var p2 = Promise.resolve()
-  .then(data => new Promise((resolve, reject) => {
-    setTimeout(Math.random() > 0.1 ? resolve : reject, 3000)
-  }));
-
-p2.then(data => console.log('Okay'));
-p2.catch(data => console.log('Oooops'));
-
-// first creates a promise fulfilled with [1, 2, 3] and then has a fulfillment // branch on top of that which maps thoes values into [2, 4, 6]. Calling .then // on that branch of the promise will produce the doubled values.
-
-var p3 = Promise.resolve([1,2,3]);
-
-p3.then(values => values.map(x => x * 2))
-  .then(values => console.log(values));
+          console.log(userInfo);
+        })
+    })
+  })
+  .catch(err => console.log(new Error('====> Ooops! Error catched <====')));
