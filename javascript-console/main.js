36b807e0c2fc5eecb88c4e90590994ba939bacb7
Simon Nonnis <simononnis@gmail.com>
2016-07-10 11:18:10 +0200

    'console.info/error/warn/group'

---
+++ javascript-console/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('this is a log');
+console.warn('this is a warning');
+console.error('this is an error');
+console.info('this is info');
+//console.debug('this is a debug'); available in browser not in node
+
+console.log('hello', 'world', '!');
+console.log({foo: 'bar'});
+console.log('hi my name is %s and i love %s', 'Simon', 'JS');
+// console.log('this is %coutput', 'color: orange');
+
+// You could use \t but the correct way is console.group/groupCollapsed
+// for (let i = 0; i < 100; i++) {
+//   var num = Math.random() * 100;
+//   console.group('Picking a random number');
+//     console.log('Number greater than 10?', num > 10);
+//     console.log('Number greater than 50?', num > 50);
+//     console.log(i);
+//   console.groupEnd();
+// }
